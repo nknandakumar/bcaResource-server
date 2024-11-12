038814185004233b44e81ef129c17de8a205eccc
nknandakuamr <tec.thunder2022nk@gmail.com>
2024-11-12 21:09:03 +0530

    3rd commit

--- db.js
+++ db.js
@@ -8,6 +8,7 @@ const db = new pg.Client({
    database: process.env.DB_NAME ,
    password: process.env.DB_PASSWORD ,
    port: process.env.DB_PORT
+   ssl: { rejectUnauthorized: false }
 })
 
 db.connect();
