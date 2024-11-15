cc3695cbe06b3550f623348b1be7142cd29cd3ef
nknandakuamr <tec.thunder2022nk@gmail.com>
2024-11-15 12:42:20 +0530

    15-11-2024

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const db = new pg.Client({
    user: process.env.DB_USER ,
-  
+   host:process.env.DB_HOST,
    database: process.env.DB_NAME ,
    password: process.env.DB_PASSWORD ,
    port: process.env.DB_PORT
